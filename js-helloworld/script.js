c231c1c8958675795d28e155332d25c25704ba4c
Giorgia Ballante <46818276+giorgiaballante@users.noreply.github.com>
2019-04-04 12:41:25 +0200

    update everything

---
+++ js-helloworld/script.js
@@ -0,0 +1,13 @@
+// single line comment
+
+/* multi
+line
+comment */
+
+// to log information to the console
+// we can use the console.log(); function
+
+console.log('hello world');
+// single line instruction - statement
+// has to end with a semicolon - ; -
+
